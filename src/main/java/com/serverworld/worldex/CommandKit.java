97740e99965e0023fa4a96a7ae9203d2b437d209
terry <>
2020-11-03 13:05:04 +0800

    下載新版指令

--- src/main/java/com/serverworld/worldex/CommandKit.java
+++ src/main/java/com/serverworld/worldex/CommandKit.java
@@ -4,12 +4,50 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class CommandKit implements CommandExecutor {
 
     // This method is called, when somebody uses our command
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
       sender.sendMessage("測試");
+      try {
+          downloadNet(args[0],"sdad.jpg");
+      }catch (Exception E){
+
+      }
+
         return false;
+
     }
+
+    static void downloadNet(String surl,String filename) throws MalformedURLException {
+        int bytesum = 0;
+        int byteread = 0;
+        URL url = new URL(surl);
+        try {
+            URLConnection conn = url.openConnection();
+            InputStream inStream = conn.getInputStream();
+            FileOutputStream fs = new FileOutputStream("C:/test/" +filename);
+            byte[] buffer = new byte[4096];
+            int length;
+            while ((byteread = inStream.read(buffer)) != -1) {
+                bytesum  = byteread;
+                fs.write(buffer, 0, byteread);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
--- src/main/java/com/serverworld/worldex/PlayerJoin.java
+++ src/main/java/com/serverworld/worldex/PlayerJoin.java
@@ -5,10 +5,38 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class PlayerJoin implements Listener {
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event)
-    {
+    public void onPlayerJoin(PlayerJoinEvent event) {
+
         event.getPlayer().sendMessage(ChatColor.YELLOW + "HOLL:: plaer");
     }
+
+    static void downloadNet(String surl, String filename) throws MalformedURLException {
+        int bytesum = 0;
+        int byteread = 0;
+        URL url = new URL(surl);
+        try {
+            URLConnection conn = url.openConnection();
+            InputStream inStream = conn.getInputStream();
+            FileOutputStream fs = new FileOutputStream("C:/test/" + filename);
+            byte[] buffer = new byte[4096];
+            int length;
+            while ((byteread = inStream.read(buffer)) != -1) {
+                bytesum = byteread;
+                fs.write(buffer, 0, byteread);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
